629d6d8a92c81072c05ad45f551e8a2c03f03a71
Jim Gay <jim@saturnflyer.com>
2010-12-14 17:48:14 -0500

    adding missing method

--- lib/interrogator.rb
+++ lib/interrogator.rb
@@ -10,4 +10,20 @@ module Interrogator
     }
     @h
   end
+  def associated_models_hash
+    result = {
+      :has_many => [],
+      :has_one => []
+      :belongs_to => []
+    }
+    reflect_on_all_associations(:has_many).each do |reflection|
+      result[:has_many] << {:name => reflection.name, :options => reflection.options}
+  end
+  reflect_on_all_associations(:has_one).each do |reflection|
+    result[:has_one] << {:name => reflection.name, :options => reflection.options}
+  end
+  reflect_on_all_associations(:belongs_to).each do |reflection|
+    result[:has_many] << {:name => reflection.name, :options => reflection.options}
+  end
+  result
 end

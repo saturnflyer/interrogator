44a31fbbc01411122fb24b67122c281b06ff625a
Jim Gay <jim@saturnflyer.com>
2011-01-05 12:21:12 -0500

    bumping version, adding Searchable class

---
+++ lib/interrogator/searchable.rb
@@ -0,0 +1,30 @@
+module Interrogator
+  class Searchable
+  
+    class_inheritable_accessor :excluded_models
+    self.excluded_models ||= []
+    
+    Rails.configuration.load_paths.map{|p| p.sub(/\/$/,'')}.uniq.each do |load_path|
+      Dir.glob(File.join(load_path, '**', 'models', '**', '*.rb')).each { |file|
+        unless file.match(/test|spec/) 
+          require file
+        end
+      }
+    end
+  
+    class << self
+      def models(opts={})
+        excluded = excluded_models
+        if opts[:exclude]
+          excluded << opts[:exclude]
+          excluded.uniq!
+        end
+        (Object.subclasses_of(ActiveRecord::Base).delete_if{ |klass| 
+          excluded.any?{ |e| 
+            e.to_s == klass.to_s 
+          }
+        }).sort_by{|klass| klass.name}
+      end
+    end
+  end
+end

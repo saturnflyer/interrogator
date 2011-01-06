367b26cc6182953450a2d1622f4dc1e9c3dc89a9
Jim Gay <jim@saturnflyer.com>
2011-01-05 22:36:52 -0500

    less rails-reload-breaky

--- lib/interrogator/searchable.rb
+++ lib/interrogator/searchable.rb
@@ -5,9 +5,10 @@ module Interrogator
     self.excluded_models ||= []
     
     Rails.configuration.load_paths.map{|p| p.sub(/\/$/,'')}.uniq.delete_if{|p| !p.match(/models/)}.each do |load_path|
-      Dir.glob(File.join(load_path, '**', 'models', '**', '*.rb')).each { |file|
-        unless file.match(/test|spec/) 
-          require file
+      Dir.glob(File.join(load_path, '**', '*.rb')).each { |file|
+        unless file.match(/test|spec/)
+          class_name = file.sub(load_path + File::SEPARATOR,'').sub(/\.rb$/,'').camelize
+          klass = class_name.split('::').inject(Object){ |klass,part| klass.const_get(part) } rescue nil
         end
       }
     end

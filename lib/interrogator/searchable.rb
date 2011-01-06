5c9d2ca3442f35fc896493a3861beff7336022e8
Jim Gay <jim@saturnflyer.com>
2011-01-06 00:00:52 -0500

    prevent models from disappearing in dev mode

--- lib/interrogator/searchable.rb
+++ lib/interrogator/searchable.rb
@@ -1,17 +1,7 @@
 module Interrogator
   class Searchable
-  
     class_inheritable_accessor :excluded_models
     self.excluded_models ||= []
-    
-    Rails.configuration.load_paths.map{|p| p.sub(/\/$/,'')}.uniq.delete_if{|p| !p.match(/models/)}.each do |load_path|
-      Dir.glob(File.join(load_path, '**', '*.rb')).each { |file|
-        unless file.match(/test|spec/)
-          class_name = file.sub(load_path + File::SEPARATOR,'').sub(/\.rb$/,'').camelize
-          klass = class_name.split('::').inject(Object){ |klass,part| klass.const_get(part) } rescue nil
-        end
-      }
-    end
   
     class << self
       def models(opts={})
@@ -20,11 +10,23 @@ module Interrogator
           excluded << opts[:exclude]
           excluded.uniq!
         end
-        (Object.subclasses_of(ActiveRecord::Base).delete_if{ |klass| 
+        load_models
+        (Object.subclasses_of(ActiveRecord::Base).delete_if{ |klass|
           excluded.any?{ |e| 
             e.to_s == klass.to_s 
           }
         }).sort_by{|klass| klass.name}
+      end
+      
+      def load_models
+        Rails.configuration.load_paths.map{|p| p.sub(/\/$/,'')}.uniq.delete_if{|p| !p.match(/models/)}.each do |load_path|
+          Dir.glob(File.join(load_path, '**', '*.rb')).each { |file|
+            unless file.match(/test|spec/)
+              class_name = file.sub(load_path + File::SEPARATOR,'').sub(/\.rb$/,'').camelize
+              klass = class_name.split('::').inject(Object){ |klass,part| klass.const_get(part) } rescue nil
+            end
+          }
+        end
       end
     end
   end

a2d3e40ec5e1056d9862c953b35ed281c61a6560
Jim Gay <jim@saturnflyer.com>
2011-07-06 15:04:19 -0400

    remove errant puts

--- lib/interrogator.rb
+++ lib/interrogator.rb
@@ -5,7 +5,4 @@ begin
 rescue LoadError
   STDERR.puts "Interrogator uses Searchlogic to provide query options. You'll need to define these yourself if you don't intend to use Searchlogic"
 end
-
-puts "EXTENDING ActiveRecord"
-
 ActiveRecord::Base.extend(Interrogator::Detailer)

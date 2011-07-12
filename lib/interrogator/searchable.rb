69bc340cecb63c0e7fd491f5de083ca939893783
Jim Gay <jim@saturnflyer.com>
2011-07-12 10:53:48 -0400

    better handling for namespaced models

--- lib/interrogator/searchable.rb
+++ lib/interrogator/searchable.rb
@@ -21,8 +21,9 @@ module Interrogator
       def load_models
         Rails.configuration.load_paths.map{|p| p.sub(/\/$/,'')}.uniq.delete_if{|p| !p.match(/models/)}.each do |load_path|
           Dir.glob(File.join(load_path, '**', '*.rb')).each { |file|
-            unless file.match(/test|spec/)
-              class_name = file.sub(load_path + File::SEPARATOR,'').sub(/\.rb$/,'').camelize
+            unless file.match(/(test|spec)$/)
+              short_file_name = file.split("models#{File::SEPARATOR}").last
+              class_name = short_file_name.sub(/\.rb$/,'').camelize
               klass = class_name.split('::').inject(Object){ |klass,part| klass.const_get(part) } rescue nil
             end
           }
